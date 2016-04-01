a6dea53d972d19dcaee17b0db2ba0341c98f97a0
jlramosr <jlramosr@gmail.com>
2016-04-01 17:46:29 +0200

    Avanznado con login pero falla google y falta mostrar mensaje y estudiar mas a fondo databinding con notify, llaves y corchetes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,22 +4,24 @@
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  let myApp = document.querySelector('#app');
+  let zrickrApp = document.querySelector('#app');
 
   // Sets app default base URL
-  myApp.baseUrl = '/';
+  zrickrApp.baseUrl = '/';
   if (window.location.port === '') {  // if production
-    // Uncomment myApp.baseURL below and
-    // set myApp.baseURL to '/your-pathname/' if running from folder in production
-    // myApp.baseUrl = '/polymer-starter-kit/';
+    // Uncomment zrickrApp.baseURL below and
+    // set zrickrApp.baseURL to '/your-pathname/' if running from folder in production
+    // zrickrApp.baseUrl = '/polymer-starter-kit/';
   }
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  myApp.addEventListener('dom-change', () => {
+  zrickrApp.addEventListener('dom-change', () => {
     let userAuth = document.querySelector('user-auth');
+    let zrickrUser;
     let currentElement;
 
+    //Main Switched Event
     userAuth.addEventListener('user-changed', (event) => {
       if (currentElement) {
         currentElement.remove();
@@ -28,33 +30,59 @@
 
       if (event.detail.value) {
         currentElement = document.createElement('zrickr-app');
-      } else {
+        zrickrUser = zrickrApp.user;
+      }
+      else {
         currentElement = document.createElement('zrickr-login');
       }
 
       document.body.appendChild(currentElement);
     });
 
+    window.addEventListener('input', (event) => {
+    });
+
+    window.addEventListener('change', (event) => {
+    });
+
+    userAuth.addEventListener('message-changed', (e) => {
+      console.log("Message UserAuth changed " + e.detail.value);
+    });
+
+    //User Login Events
     window.addEventListener('login-tapped', (event) => {
-      console.log("event.detail", event.detail);
-      console.log("userAuth ", userAuth);
-      userAuth.provider = event.detail.provider;
-      userAuth.email = event.detail.email;
-      userAuth.password = event.detail.password;
+      let auth = event.detail;
+      userAuth.provider = auth.provider;
+      userAuth.email = auth.email;
+      userAuth.password = auth.password;
       userAuth.login();
     });
-
-    window.addEventListener('input', (event) => {
-      console.log(event.detail);
+    window.addEventListener('signin-tapped', (event) => {
+      let auth = event.detail;
+      userAuth.provider = auth.provider;
+      userAuth.email = auth.email;
+      userAuth.password = auth.password;
+      userAuth.createUser();
+    });
+    window.addEventListener('reset-password-tapped', (event) => {
+      userAuth.email = event.detail.email;
+      userAuth.resetPassword();
     });
 
+    //User App Events
     window.addEventListener('logout-tapped', (event) => {
       userAuth.logout();
     });
-
-    window.addEventListener('signin-tapped', (event) => {
-      userAuth.createUser();
+    window.addEventListener('change-password-tapped', (event) => {
+      let auth = event.detail;
+      console.log(auth.oldPassword, auth.newPassword)
+      userAuth.email = zrickrUser.password.email;
+      userAuth.changePassword(auth.oldPassword, auth.newPassword);
     });
+    window.addEventListener('signout-tapped', (event) => {
+      userAuth.removeUser();
+    });
+
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
